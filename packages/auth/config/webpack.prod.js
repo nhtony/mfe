f4b771d5519984765ba3fc7d50cdf24a1218b778
nhtony <honhathao062@gmail.com>
2021-01-01 13:01:08 +0700

    added auth subapp

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -4,23 +4,17 @@ const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
 
 const devConfig = {
-    mode: 'development',
+    mode: 'production',
     output: {
-        publicPath: 'http://localhost:8080/'
+        filename: '[name].[contenthash].js',
+        publicPath: '/auth/latest/',
     },
-    devServer: {
-        port: 8080,
-        historyApiFallback: {
-            index: 'index.html'
-        }
-    },
-
     plugins: [
         new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-                auth: 'auth@http://localhost:8082/remoteEntry.js',
+            name: 'auth',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './AuthApp': './src/bootstrap'
             },
             shared: packageJson.dependencies
         })
